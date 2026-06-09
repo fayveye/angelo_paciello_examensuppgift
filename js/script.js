0c53f1f8d50460efb796ff6540a0d6c5fd182311
fayveye <freakythinking85@gmail.com>
2026-06-09 15:38:29 +0200

    i CHANGED EVERYTHING
    theres a play music button (ö) now theres an actual keybored now that looks somewhat good the keys change color when clicked now

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+//turns the A key red when A key is held down
+document.addEventListener("keydown", (event) => {
+
+let pressedKey = event.key.toLowerCase(); 
+    document.getElementById(pressedKey).style = "background-color:darkgrey";
+});
+
+
+////////////////////////GONNA KILL MYSELF/////////////////////////////////
+
+
+document.addEventListener("keyup", (event) => {
+
+let pressedKey = event.key.toLowerCase(); 
+    document.getElementById(pressedKey).style = "background-color:white";
+});
+
+/* play music on M key
+
+const audio = document.createElement("audio")
+audio.src = "filer/PortalRadio.ogg" ;
+
+
+const musicbutton = document.getElementById("M")
+musicbutton.addEventListener("click", playmusic);
+
+function playmusic(){
+    audio.play();
+    }
+    */
+
+    console.log("det fungerar")
+
+
+    document.addEventListener("keydown", (event) => {
+    
+    console.log(event.key);
+    
+    const sound = new Audio("filer/PortalRadio.ogg")
+    
+    if(event.key == "ö") {
+    sound.currentTime = 0;
+    
+    sound.play();
+    
+    }})
+
+
+
+
+
+    
+
+
+
+
+
